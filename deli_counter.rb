314621f3d31e34abcd490be0b350560e71029ec9
Matt Wolford <matthew.r.wolford@gmail.com>
2021-02-16 02:07:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,6 +21,9 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
+  if katz_deli == "[]"
+    puts "There is nobody waiting to be served!"
+
   puts "Currently serving #{katz_deli.shift}."
 end
 
